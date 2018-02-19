1ebc053c85531e5221caee4e727ea27f17eebdd1
mrivera316 <35825749+mrivera316@users.noreply.github.com>
2018-02-18 21:10:17 -0600

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,54 @@
+function getCookie(c_name)
+{
+var i,x,y,ARRcookies=document.cookie.split(";");
+for (i=0;i<ARRcookies.length;i++)
+  {
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x=x.replace(/^\s+|\s+$/g,"");
+  if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
+
+function setCookie(c_name,value,exdays)
+{
+var exdate=new Date();
+exdate.setDate(exdate.getDate() + exdays);
+var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+document.cookie=c_name + "=" + c_value;
+}
+
+function checkCookie()
+{
+var username=getCookie("test3");
+if (username!=null && username!="")
+  {
+  alert("I see you there " + username);
+  }
+else
+  {
+  username=prompt("Why don't you tell me your name:","");
+  if (username!=null && username!="")
+    {
+    setCookie("test3",username,30);
+    }
+  }
+}
+
+function removeCookie() {
+	setCookie("test3","",-1)
+}
+
+var w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var h = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+var x = document.getElementById("button");
+x.innerHTML = "Browser inner window width: " + w + ", height: " + h + ".";
